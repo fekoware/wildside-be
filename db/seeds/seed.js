6e105bb99bcd1ba26a0fc71387662f2661757ef0
Dan Olver <devdolphin7@outlook.com>
2024-08-16 15:38:35 +0100

    clear out tables, add and populate users

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,41 @@
-const format = require('pg-format')
-
-const db = require('../connection.js')
+const format = require("pg-format");
+const db = require("../connection.js");
 
 const seed = () => {
-    
+  return (
+    db
+      // Clear out tables
+      .query("DROP TABLE IF EXISTS users")
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS sightings");
+      })
+      .then(() => {
+        return db.query("DROP TABLE IF EXISTS favourite_wildlife");
+      })
+      // Re-create tables
+      .then(() => {
+        return createUsers();
+      })
+      // Populate tables
+      .then(() => {
+        return insertUsers();
+      })
+  );
+};
+
+function createUsers() {
+  // -----Password ticket raised https://trello.com/c/XsiShEew/68-password-security-spike-------
+  return db.query(`CREATE TABLE users ()
+        user_id SERIAL PRIMARY KEY,
+        username VARCHAR(40) NOT NULL,
+        password VARCHAR(40) NOT NULL,
+        email NVARCHAR(320) NOT NULL,
+        my_sightings INT REFERENCES sightings(sighting_id)
+        favourite_wildlife INT REFERENCES favourite_wildlife(wildlife_id)`);
 }
+
+function insertUsers() {
+  return;
+}
+
+module.exports = seed
