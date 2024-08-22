b78d1fc20ba91703acd26bb6de07d559b8f5778e
Mayowa Olufeko <mayfeko@gmail.com>
2024-08-22 12:07:52 +0100

    setting up database hosting on supabase

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,18 @@
 const { Pool } = require("pg");
+require("dotenv").config({ path: envPath });
+
 const ENV = process.env.NODE_ENV || "development";
+
 const envPath = `${__dirname}/../.env.${ENV}`;
 
-require("dotenv").config({ path: envPath });
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+
 
 if (!process.env.PGDATABASE) {
   throw new Error("no pg database configured");
