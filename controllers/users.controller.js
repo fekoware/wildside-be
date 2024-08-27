39663506400e76dcf53f0b458957b92eeda21320
Dan Olver <devdolphin7@outlook.com>
2024-08-27 15:03:29 +0100

    Add postLoginAttempt controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,35 +1,50 @@
-const { insertUser, selectUsers, selectUserByUsername } = require("../models/users.model")
+const {
+  insertUser,
+  selectUsers,
+  selectUserByUsername,
+  attemptLogin,
+} = require("../models/users.model");
 
 exports.postUser = (request, response, next) => {
-    const user = request.body;
-    insertUser(user).then((newUser) => {
-        const userResponse = {
-            user_id: newUser.user_id,
-            username: newUser.username,
-            email: newUser.email,
-        };
-        response.status(201).send({ user: userResponse })
+  const user = request.body;
+  insertUser(user)
+    .then((newUser) => {
+      const userResponse = {
+        user_id: newUser.user_id,
+        username: newUser.username,
+        email: newUser.email,
+      };
+      response.status(201).send({ user: userResponse });
     })
-        .catch(next)
-}
+    .catch(next);
+};
 
 exports.getUsers = (request, response, next) => {
-    selectUsers()
-        .then((users) => {
-            response.status(200).send({ users });
-        })
-        .catch(next)
-}
+  selectUsers()
+    .then((users) => {
+      response.status(200).send({ users });
+    })
+    .catch(next);
+};
 
 exports.getUserByUsername = (request, response, next) => {
-    const { username } = request.params;
+  const { username } = request.params;
 
-    selectUserByUsername(username)
-        .then((user) => {
-            if (!user) {
-                return response.status(404).send({ message: "User not found" });
-            }
-            response.status(200).send({ user });
-        })
-        .catch(next);
+  selectUserByUsername(username)
+    .then((user) => {
+      if (!user) {
+        return response.status(404).send({ message: "User not found" });
+      }
+      response.status(200).send({ user });
+    })
+    .catch(next);
+};
+
+exports.postLoginAttempt = (request, response, next) => {
+  const loginAttempt = request.body;
+  attemptLogin(loginAttempt)
+    .then((user) => {
+      response.send({ user });
+    })
+    .catch(next);
 };
